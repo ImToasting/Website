cd64c4fc365e718de3a4f1fc4a24e14828e4ee97
Sreeman Reddy <hi@sreeman.sh>
2025-12-18 17:09:22 +0530

    fix: switch to adapter-auto and clean deps

--- svelte.config.js
+++ svelte.config.js
@@ -3,22 +3,12 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
-	extensions: [".svelte", ".svg"],
+	extensions: ['.svelte', '.svg'],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: true
-		}),
+		adapter: adapter(),
+
 		alias: {
 			'@assets': './src/assets',
 			'@components': './src/components',
@@ -26,8 +16,8 @@ const config = {
 			'@partials': './src/partials',
 			'@sections': './src/sections',
 			'@styles': './src/styles',
-			'@types': './src/types',
-		},
+			'@types': './src/types'
+		}
 	}
 };
 
