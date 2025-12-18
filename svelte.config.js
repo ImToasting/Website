b95649bb07278fcb23f618089f42cf35f912bbd5
Sreeman Reddy <hi@sreeman.sh>
2025-12-18 16:36:58 +0530

    Website v3

---
+++ svelte.config.js
@@ -0,0 +1,34 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	extensions: [".svelte", ".svg"],
+
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: true
+		}),
+		alias: {
+			'@assets': './src/assets',
+			'@components': './src/components',
+			'@config': './src/config',
+			'@partials': './src/partials',
+			'@sections': './src/sections',
+			'@styles': './src/styles',
+			'@types': './src/types',
+		},
+	}
+};
+
+export default config;
